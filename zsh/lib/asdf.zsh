2c369e48f56590afcba55c02fa878e68d0fb2dac
Rafael Gumieri <rgumieri@gmail.com>
2019-05-20 01:52:59 -0300

    asdf: configure to use $XDG_CONFIG_HOME

--- zsh/lib/asdf.zsh
+++ zsh/lib/asdf.zsh
@@ -1,2 +1,3 @@
-source "${HOME}/.asdf/asdf.sh"
-source "${HOME}/.asdf/completions/asdf.bash"
+export ASDF_DATA_DIR=$XDG_CONFIG_HOME/asdf
+source "${ASDF_DATA_DIR}/asdf.sh"
+source "${ASDF_DATA_DIR}/completions/asdf.bash"
