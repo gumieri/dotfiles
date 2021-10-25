73de7c757862f5d5a19333dba31b044f4f9a4a1f
Rafael Gumieri <rafael.gumieri@tecnospeed.com.br>
2021-10-25 10:27:49 -0300

    remove fzf

--- zsh/lib/key-bindings.zsh
+++ zsh/lib/key-bindings.zsh
@@ -69,9 +69,9 @@ bindkey '\C-x\C-e' edit-command-line
 # file rename magick
 bindkey "^[m" copy-prev-shell-word
 
-[[ $- == *i* ]] && source "${XDG_CONFIG_HOME}/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/share/fzf/key-bindings.zsh" 2> /dev/null
 
-source "${XDG_CONFIG_HOME}/fzf/shell/key-bindings.zsh"
+source "/usr/share/fzf/key-bindings.zsh"
 
 function open-project-path {
   local project=$(p | fzf)
